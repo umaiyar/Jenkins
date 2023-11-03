6365a04c53725755b3637ad5d4b0cb0d52da8a5f
Umaiyar <143181444+umaiyar@users.noreply.github.com>
2023-11-03 16:22:18 +0530

    Create jenkinscode.java

---
+++ jenkinscode.java
@@ -0,0 +1,10 @@
+public class second {
+
+	public static void main(String[] args) {
+		System.out.println("Happy Coding Monday");
+  	System.out.println("Happy Coding Tuesday");
+  	System.out.println("Happy Coding Wednesday");
+
+	}
+
+}
